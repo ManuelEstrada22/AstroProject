48968b11f7dd10ebb24b35bff9ea5bbedb063bee
Estrada <alejandroeshe22@gmail.com>
2024-03-13 16:36:17 -0600

    feature

---
+++ tailwind.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.astro'],
+	theme: {
+		extend: {
+			colors:{
+				prYellow:"#DBF277",
+				prBlue:"#155e75",
+			}
+		},
+	},
+	plugins: [],
+}
